e9c995eb797cd133e83a9242a2058c3f4421f879
deeppaz <deeppaz@hotmail.com>
2020-03-15 23:16:41 +0300

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+//uygulamayı daha iyi kontrol etmek için tip kontrolu(proptypes) componentini kullanıyoruz
+import PropTypes from 'prop-types';
 
 //material ui işlevleri
+import withStyles from '@material-ui/core/styles/withStyles';
 import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const drawerWidth = 240;
+
+//stiller
+const styles = {
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    header: {
+        marginLeft: "15%"
+    },
+};
+
 
 class Navbar extends Component {
     render() {
+        const { classes } = this.props;
         return (
-            <AppBar>
-                <Toolbar className="nav-container">
-                    <Button color="inherit" component={Link} to="/giris">Giriş</Button>
-                    <Button color="inherit" component={Link} to="/">Anasayfa</Button>
-                    <Button color="inherit" component={Link} to="/uyeol">Uye Ol</Button>
-                </Toolbar>
-            </AppBar>
+            <div>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <Typography variant="h6" noWrap className={classes.header}>
+                            Raporlar İstatislikler
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                {/* left-navbar yaratıyoruz, materialui'ın sunduğu paper modulu ile */}
+                <Drawer
+                    className={classes.drawer}
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <h1>hello</h1>
+                </Drawer>
+            </div>
         )
     }
 }
 
-export default Navbar;
+//proptypes ile materialui'ın sunduğu withstyles'tan objeleri çekiyoruz classes adı ile
+Navbar.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+//burada export ediyoruz withstylesı styles'a bağlıyoruz. Navbar component adı
+export default (withStyles(styles)(Navbar));
